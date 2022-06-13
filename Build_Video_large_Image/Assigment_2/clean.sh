d270c19ea008725ba30e0f92c31ff124b21ee197
Lunga Tsewu <81965280+tswlun002@users.noreply.github.com>
2022-06-13 04:33:45 +0200

    ...

--- Build_Video_large_Image/Assigment_2/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
-#rm /home/lunga/.android/avd/Pixel_3a_API_30_x86.avd/*.lock
- 
--- Build_Video_large_Image/Assigment_2/makeVideo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#convert  pgm to pgn image
-for i in *.pgm;
-  do name=`echo "$i" | cut -d'.' -f1`
-  echo "$name"
-  ffmpeg -i "$i" "${name}.png"
-done
-#make video
-ffmpeg -framerate 15 -pattern_type  glob -i "*.png" video.mp4
